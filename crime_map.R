c3a4adb99132f156df1f265b31ba09c6cf8520e6
corynissen <corynissen@gmail.com>
2013-05-15 16:23:53 -0500

    added ggsave lines

--- crime_map.R
+++ crime_map.R
@@ -20,7 +20,7 @@ df <- subset(df, !is.na(longitude) & !is.na(latitude))
 
 # the shape of chicago, no underlying shape, just x,y points
 ggplot(df) + geom_point(aes(x=longitude, y=latitude), alpha=.2, size=.9)
-
+ggsave("chicago_crime_map_no_underlay.jpg")
 
 # now, let's put the shape of chicago underneath the points
 rlat <- range(df$latitude)
@@ -29,3 +29,4 @@ merc <- projectMercator(df$latitude, df$longitude) #OSM uses mercator
 # give openmap the upper right and lower left corners you want for your map
 mp <- openmap(c(rlat[2], rlon[1]), c(rlat[1], rlon[2]), type="osm")
 autoplot(mp) + geom_point(aes(x=merc[,1], y=merc[,2]), alpha=.1, size=.9)
+ggsave("chicago_crime_map_osm_underlay.jpg")
